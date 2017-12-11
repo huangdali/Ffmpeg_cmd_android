43c193f65550d4e0de5d30b93f80dc6d7eafad03
huangdali <137722697@qq.com>
2017-12-11 18:08:48 +0800

    v1.0.2视频格式转换

--- app/src/main/java/com/hdl/cmd/MainActivity.java
+++ app/src/main/java/com/hdl/cmd/MainActivity.java
@@ -12,25 +12,53 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Ffmpeg ffmpegCmd;
+    private static final String dir = Environment.getExternalStorageDirectory().getPath() + File.separator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ffmpegCmd = new Ffmpeg();
     }
+
+    private long curTime;
 
     public void onStartRun(View view) {
         try {
-            Ffmpeg ffmpegCmd = new Ffmpeg();
+
             /**
              * 将sdcard中的test.ts文件转换成为output_ts_to_1001.mp4并输出
              */
-            final String dir = Environment.getExternalStorageDirectory().getPath() + File.separator;
-            String cmd = ("ffmpeg -i " + dir + "test.ts -acodec copy -vcodec copy -absf aac_adtstoasc " + dir + "output_ts_to_1001.mp4");
-            ELog.e(cmd);
-            int execute = ffmpegCmd.execute(cmd.split(" "));
-            ELog.e("执行完毕了" + execute);
+            ffmpegCmd.transform(dir + "11/11.ts", dir + "11/output_ts_to_1105.mp4", new Ffmpeg.OnExecuteListener() {
+                @Override
+                public void onStart() {
+                    ELog.e("开始了");
+                    curTime = System.currentTimeMillis();
+                }
+
+                @Override
+                public void onFinished(int result) {
+                    ELog.e("耗时：" + (System.currentTimeMillis() - curTime) / 1000f + " s");
+                    ELog.e("结束了" + result);
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void onAddWaterMark(View view) {
+        ffmpegCmd.addWatermark(dir + "11/output_ts_to_1105.mp4", dir + "11/t.png", dir + "11/output_watermark_004.mp4", new Ffmpeg.OnExecuteListener() {
+            @Override
+            public void onStart() {
+                ELog.e("开始了");
+            }
+
+            @Override
+            public void onFinished(int result) {
+                ELog.e("结束了" + result);
+            }
+        });
     }
 }
--- ffmpeg_lib/src/main/java/com/hdl/ffmpeg/Ffmpeg.java
+++ ffmpeg_lib/src/main/java/com/hdl/ffmpeg/Ffmpeg.java
@@ -1,5 +1,7 @@
 package com.hdl.ffmpeg;
 
+import android.text.TextUtils;
+
 /**
  * Created by HDL on 2017/12/8.
  *
@@ -13,6 +15,7 @@ public class Ffmpeg {
         System.loadLibrary("avformat-57");
         System.loadLibrary("avutil-55");
         System.loadLibrary("swscale-4");
+        System.loadLibrary("avfilter-6");
     }
 
     /**
@@ -21,5 +24,79 @@ public class Ffmpeg {
      * @param cmd
      * @return
      */
-    public native int execute(String[] cmd);
+    private native int execute(String[] cmd);
+
+    /**
+     * 视频格式转换【内部已经使用子线程，不要过多同时调用次方法】
+     *
+     * @param sourceFile 源文件（需要转换的文件），不能包含空格
+     * @param targetFile 目标文件（转换之后的文件），不能包含空格
+     * @param listener
+     * @throws Exception
+     */
+    public void transform(String sourceFile, String targetFile, final OnExecuteListener listener) throws Exception {
+        if (listener != null) {
+            listener.onStart();
+        }
+        if (TextUtils.isEmpty(sourceFile) || TextUtils.isEmpty(targetFile)) {
+            if (listener != null) {
+                listener.onFinished(-1);
+            }
+            return;
+        }
+        final String cmd = ("ffmpeg -i " + sourceFile.trim() + " -acodec copy -vcodec copy -absf aac_adtstoasc " + targetFile.trim());
+
+        new Thread() {
+            @Override
+            public void run() {
+                int execute = execute(cmd.split(" "));
+                if (listener != null) {
+                    listener.onFinished(execute);
+                }
+            }
+        }.start();
+
+    }
+
+    /**
+     * 添加水印
+     *
+     * @param sourceFile    源文件（需要转换的文件），不能包含空格
+     * @param targetFile    目标文件（转换之后的文件），不能包含空格
+     * @param watermarkFile 水印地址，不能包含有空格
+     * @param listener
+     */
+    public void addWatermark(String sourceFile, String watermarkFile, String targetFile, final OnExecuteListener listener) {
+        if (listener != null) {
+            listener.onStart();
+        }
+        if (TextUtils.isEmpty(sourceFile) || TextUtils.isEmpty(targetFile)) {
+            if (listener != null) {
+                listener.onFinished(-1);
+            }
+            return;
+        }
+//        final String cmd = ("ffmpeg -y -i " + sourceFile.trim() + " -i "+watermarkFile+" -filter_complex [0:v][1:v]overlay=main_w-overlay_w-10:main_h-overlay_h-10[out] -map [out] -map 0:a -codec:a copy " + targetFile.trim());
+        final String cmd = ("ffmpeg -i " + sourceFile.trim() + " -vf \"movie="+watermarkFile.trim()+"[watermark];[in][watermark] overlay=10:10[out]\" " + targetFile.trim());
+
+        new Thread() {
+            @Override
+            public void run() {
+                int execute = execute(cmd.split(" "));
+                if (listener != null) {
+                    listener.onFinished(execute);
+                }
+            }
+        }.start();
+
+    }
+
+    /**
+     * 执行回调
+     */
+    public interface OnExecuteListener {
+        void onStart();
+
+        void onFinished(int result);
+    }
 }
